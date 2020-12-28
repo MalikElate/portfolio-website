34f80fccd30b530a41758bf370495e8c91e63e63
Malik Elate <Malik.e1955@gmail.com>
2020-12-28 10:40:03 -0600

    push before heroku init push

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -24,38 +24,40 @@ class Project extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Card className={classes.card, 'card'}>
-        <CardActionArea onClick={() => {window.open(this.props.projectInfo.demoLink);}}>
-          <CardMedia
-            component="img"
-            alt={this.props.projectInfo.title}
-            className={classes.media}
-            height="5%"
-            image={this.props.projectInfo.image}
-            title="Contemplative Reptile"
-            />
-            </CardActionArea>
-            <CardContent>
-            <Typography gutterBottom variant="h6" component="h2">
-              {this.props.projectInfo.title}
-            </Typography>
-            <Typography component="p">
-            {this.props.projectInfo.description}
-            </Typography>
-          </CardContent>
-        <CardActions>
-            <a target="_blank" className="social-link-a" href={this.props.projectInfo.githubLink} rel="noopener noreferrer">
-              <Button size="small" color="primary">
-                  Code
-              </Button>
-            </a>
-            <a target="_blank" className="social-link-a" href={this.props.projectInfo.demoLink} rel="noopener noreferrer">
-              <Button size="small" color="primary">
-                Demo
-              </Button>
-            </a>
-        </CardActions>
-    </Card>
+      <span className='card'>
+        <Card className={classes.card}>
+          <CardActionArea onClick={() => {window.open(this.props.projectInfo.demoLink);}}>
+            <CardMedia
+              component="img"
+              alt={this.props.projectInfo.title}
+              className={classes.media}
+              height="5%"
+              image={this.props.projectInfo.image}
+              title="Contemplative Reptile"
+              />
+              </CardActionArea>
+              <CardContent>
+              <Typography gutterBottom variant="h6" component="h2">
+                {this.props.projectInfo.title}
+              </Typography>
+              <Typography component="p">
+              {this.props.projectInfo.description}
+              </Typography>
+            </CardContent>
+          <CardActions>
+              <a target="_blank" className="social-link-a" href={this.props.projectInfo.githubLink} rel="noopener noreferrer">
+                <Button size="small" color="primary">
+                    Code
+                </Button>
+              </a>
+              <a target="_blank" className="social-link-a" href={this.props.projectInfo.demoLink} rel="noopener noreferrer">
+                <Button size="small" color="primary">
+                  Demo
+                </Button>
+              </a>
+          </CardActions>
+      </Card>
+      </span>
     );
   }
 }
